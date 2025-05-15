4ef06eef12520407e805c9ab8d00a790a91d1e08
Cristian <piovani.cp@gmail.com>
2025-05-15 14:11:17 +0200

    rimosso uso di dipendenze eliminate precedentemente

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,7 @@
-import { Link } from "react-router";
 import "./navbar.scss";
 export default function Navbar() {
   return (
     <div className="navbar">
-      {/* <Link className="nav-item" to="/">
-        Home
-      </Link>
-      <Link className="nav-item" to="/me-&-ai">
-        ME&AI
-      </Link> */}
       <a href="/#about" className="nav-item">
         About
       </a>
